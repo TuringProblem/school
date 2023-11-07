12f69a70e1661142ce88ddc9a3c87498da1d98b8
Override <122590935+TuringProblem@users.noreply.github.com>
2023-11-07 13:44:35 -0500

    Working with For  Loops in class

---
+++ demoForLoops_H02.java
@@ -0,0 +1,25 @@
+/**
+*@author override
+*@since 11/07/2023
+*@see <a href="https://github.com/TuringProblem">GitHub Profile</a>
+*/
+public class demoForLoops_H02 {
+   public static void main(String[] args) {
+   
+   //can also declare int i outside of the loop, and then print the value of i outside as well
+       for (int i = 0; i <= 5; ++i) {
+         System.out.println(i);
+
+//          for (int i = 5; i >= 0; --i) {
+//             System.out.println(i);
+//          }
+
+//             for (int i = 0, j = 1; i < 10 && j < 10; i = i + 2, j = j * 3) {
+//                System.out.println(i + "\t" + j);
+//             } // \t is for "tab"
+      
+      
+      
+      }//end for loop
+   }//end method
+}//end class
