840638efa2a4f49625fe73574766bd1da67f3f32
Override <122590935+TuringProblem@users.noreply.github.com>
2023-11-07 13:58:52 -0500

    using \t and for loops to show ASCII value

---
+++ ASCIIForLoops_H02.java
@@ -0,0 +1,18 @@
+/**
+*@author override
+*@since 11/07/2023
+*@see <a href="https://github.com/TuringProblem">GitHub Profile</a>
+*/
+public class ASCIIForLoops_H02 {
+   public static void main(String[] args) {
+        System.out.println("#\tchar");
+   
+       for (int i = 32; i <= 128; ++i) {
+
+         System.out.println(i + "\t" + (char)i);
+
+      
+      
+      }//end for loop
+   }//end method
+}//end class
