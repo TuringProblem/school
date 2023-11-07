6493c144e275f9f7d03bdc3bf9466fab09b29b74
Override <122590935+TuringProblem@users.noreply.github.com>
2023-11-07 13:58:36 -0500

    Working with more for loop

---
+++ golfForLoop.java
@@ -0,0 +1,19 @@
+/**
+*@author override
+*@since 11/07/2023
+*@see <a href="https://github.com/TuringProblem">GitHub Profile</a>
+*/
+public class golfForLoop_H02 {
+   public static void main(String[] args) {
+       System.out.println("#\tbet");
+       double bet = 1;
+       for (int i = 1; i <= 18; i = i + 1) {
+         
+
+         System.out.println(i + "\t" + bet);
+         bet = bet * 2;
+      
+      
+      }//end for loop
+   }//end method
+}//end class
