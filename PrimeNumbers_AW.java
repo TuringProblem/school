a2a7cb34d9fb5316d8be299bcc230a3f4341588c
Override <122590935+TuringProblem@users.noreply.github.com>
2023-11-19 03:49:12 -0500

    moving methods around

---
+++ PrimeNumbers_AW.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+/**
+ * @author Override
+ * @since   11/19/2023 03:48
+ * @see <a href="https://github.com/TuringProblem">GitHub Profile</a>
+ */
+
+
+public class PrimeNumbers_AW {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter your first value: ");
+        a = keyboard.nextInt();
+/*        System.out.println("Please enter your second value: ");
+        b = keyboard.nextInt();*/
+
+        System.out.println("You're number is prime: " + determineIfPrime(a));
+        System.out.println("You're sophie germain number is prime: " + isSophieGermainPrime(a));
+        System.out.println("You're twin number is prime: " + isTwinPrime(a));
+        System.out.println("You're mersenne number is prime: " + isMersennePrime(a));
+    }
+
+    public static boolean isSophieGermainPrime(int number) {
+        boolean isPrime = true;
+        if (determineIfPrime(number) && determineIfPrime(2 * number + 1)) {
+            return true;
+        }
+        return false;
+    }
+   public static boolean isTwinPrime(int number) {
+       boolean isPrime = true;
+       if (determineIfPrime(number) && determineIfPrime(number + 2)) {
+           return true;
+       }
+       return false;
+    }
+    public static boolean isMersennePrime(int number) {
+        boolean isPrime = true;
+        if (determineIfPrime(number) && determineIfPrime(2 * number - 1)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean determineIfPrime(int valuePassed) {
+        boolean isPrime = true;
+        if (valuePassed < 2) {
+            isPrime = false;
+        } else {
+            if (valuePassed == 2) {
+                isPrime = true;
+            } else {
+                for(int i = 2; i < Math.sqrt(valuePassed) + 1; ++i) {
+                    if (valuePassed % i == 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return isPrime;
+    }
+
+}
