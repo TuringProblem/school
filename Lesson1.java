84d683285afffc266471e62db1d3ca67fb6d87db
Override <122590935+TuringProblem@users.noreply.github.com>
2024-01-16 13:25:06 -0500

    Arrays

---
+++ Lesson1.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+/**
+ * @author Override
+ * @see <a href="Https://Github.com/TuringProblem">GitHub Profile</a>
+ */
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        int ages[] = {4, 5, 3, 5, 7};
+        double heights[] = {4.5, 6.3, 2.5, 7.5, 4.5};
+
+/*
+        heights[0] = 4.7;
+        heights[1] = 2.9;
+        heights[2] = 3.5;
+        heights[3] = 6.6;
+        heights[4] = 5.1;
+*/
+
+        System.out.println("The first child's height is " + heights[0]);
+        System.out.println("The first child's age is " + ages[0] + "\n");
+
+        System.out.println("The second child's height is " + heights[1]);
+        System.out.println("The second child's age is " + ages[1] + "\n");
+
+        System.out.println("The third child's height is " + heights[2]);
+        System.out.println("The third child's age is " + ages[2] + "\n");
+
+        System.out.println("The fourth child's height is " + heights[3]);
+        System.out.println("The fourth child's age is " + ages[3] + "\n");
+
+        System.out.println("The fifth child's height is " + heights[4]);
+        System.out.println("The fifth child's age is " + ages[4]);
+
+/*
+        ages[0] = 1;
+        ages[1] = 2;
+        ages[2] = 3;
+        ages[3] = 5;
+        ages[4] = 6;
+*/
+
+
+
+    }
+}
